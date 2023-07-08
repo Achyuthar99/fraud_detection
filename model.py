86e224c65502e9ba5bb083cc541dba6dcba37d69
Achyuthar99 <achyutha1ga19me401@gmail.com>
2023-07-09 00:09:24 +0530

    added  3 new model

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 print("adding all ML files")
+
+
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model2():
+    print("adding code for model3")
